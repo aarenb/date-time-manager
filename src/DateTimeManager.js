6bce31ed248e2742a9bffdb9c3fe3ce0bbe2a89a
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-12 14:07:28 +0200

    Add DateTimeManager

---
+++ src/DateTimeManager.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a DateTimeManager.
+ */
+class DateTimeManager {
+  /**
+   * Formats time into the 24 hour clock.
+   *
+   * @param {string} time - The time to format.
+   */
+  to24HourClock (time) {
+
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { DateTimeManager } from './DateTimeManager'
+
+const dateTimeManager = new DateTimeManager()
+
+console.log(dateTimeManager.to24HourClock('02:32pm'))
