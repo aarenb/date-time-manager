e47d6c3021bab69b62a0221d94c04502de7bd4d1
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-12 14:41:29 +0200

    Change how to check if it's am or pm

--- src/DateTimeManager.js
+++ src/DateTimeManager.js
@@ -5,7 +5,7 @@ export class DateTimeManager {
   /**
    * Formats time into the 24 hour clock.
    *
-   * @param {string} time - The time to format.
+   * @param {string} time - The time to format (hh:mm xm).
    * @returns {string} The time formated in the 24 hour clock.
    */
   to24HourClock (time) {
@@ -13,9 +13,9 @@ export class DateTimeManager {
       throw new TypeError('The passed argument is not a string.')
     }
 
-    if (time.includes('am')) {
+    if (time.charAt(5) === 'a' && time.charAt(6) === 'm') { // if am
       return 'am'
-    } else if (time.includes('pm')) {
+    } else if (time.charAt(5) === 'p' && time.charAt(6) === 'm') { // if pm
       return 'pm'
     } else {
       throw new TypeError('The passed argument is not a valid 12h clock timestamp.') // TODO, change error text??
