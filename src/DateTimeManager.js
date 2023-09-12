4d00a8b1f04fed37f5cfbbef57d507d1538fa22f
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-12 14:57:22 +0200

    Add comments about what to do

--- src/DateTimeManager.js
+++ src/DateTimeManager.js
@@ -14,8 +14,24 @@ export class DateTimeManager {
     }
 
     if (time.charAt(5) === 'a' && time.charAt(6) === 'm') { // if am
+      // basically just have to remove the am at the end here
+      // 12 = 00
       return 'am'
     } else if (time.charAt(5) === 'p' && time.charAt(6) === 'm') { // if pm
+      /*
+      01 = 13
+      02 = 14
+      03 = 15
+      04 = 16
+      05 = 17
+      06 = 18
+      07 = 19
+      08 = 20
+      09 = 21
+      10 = 22
+      11 = 23
+      12 = 12
+      */
       return 'pm'
     } else {
       throw new TypeError('The passed argument is not a valid 12h clock timestamp.') // TODO, change error text??
