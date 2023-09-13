7cf1eb607c9cbda6a8910e8a11ec5a151fa78257
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-13 09:16:04 +0200

    Make to24HourClock work

--- src/DateTimeManager.js
+++ src/DateTimeManager.js
@@ -13,65 +13,57 @@ export class DateTimeManager {
       throw new TypeError('The passed argument is not a string.')
     }
 
+    let newHH = ''
+    const hh = `${time.charAt(0)}${time.charAt(1)}`
+    const mm = `${time.charAt(3)}${time.charAt(4)}`
+
     if (time.charAt(5) === 'a' && time.charAt(6) === 'm') { // if am
-      // basically just have to remove the am at the end here
-      // 12 = 00
-      return 'am'
-    } else if (time.charAt(5) === 'p' && time.charAt(6) === 'm') { // if p
-      const hh = `${time.charAt(0)}${time.charAt(1)}`
+      if (hh === '12') {
+        newHH = '00'
+      } else {
+        newHH = hh
+      }
+      return `${newHH}:${mm}`
+    } else if (time.charAt(5) === 'p' && time.charAt(6) === 'm') { // if pm
       switch (hh) {
         case '01':
-          console.log('13')
+          newHH = '13'
           break
         case '02':
-          console.log('14')
+          newHH = '14'
           break
         case '03':
-          console.log('15')
+          newHH = '15'
           break
         case '04':
-          console.log('16')
+          newHH = '16'
           break
         case '05':
-          console.log('17')
+          newHH = '17'
           break
         case '06':
-          console.log('18')
+          newHH = '18'
           break
         case '07':
-          console.log('19')
+          newHH = '19'
           break
         case '08':
-          console.log('20')
+          newHH = '20'
           break
         case '09':
-          console.log('21')
+          newHH = '21'
           break
         case '10':
-          console.log('22')
+          newHH = '22'
           break
         case '11':
-          console.log('23')
+          newHH = '23'
           break
         case '12':
-          console.log('12')
+          newHH = '12'
           break
       }
-      /*
-      01 = 13
-      02 = 14
-      03 = 15
-      04 = 16
-      05 = 17
-      06 = 18
-      07 = 19
-      08 = 20
-      09 = 21
-      10 = 22
-      11 = 23
-      12 = 12
-      */
-      return 'pm'
+      return `${newHH}:${mm}`
     } else {
       throw new TypeError('The passed argument is not a valid 12h clock timestamp.') // TODO, change error text??
     }
