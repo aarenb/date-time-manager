fdfbf91f40b5ae28af19ed3919deaf01d87e5113
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-13 13:09:09 +0200

    Add another automated test

--- test/DateTimeManager.test.js
+++ test/DateTimeManager.test.js
@@ -1,7 +1,11 @@
-import { to24HourClock } from '../src/dateTimeManager.js'
+import * as dateTimeManager from '../src/dateTimeManager.js'
 
 describe('to24HourClock', () => {
   test('"03:22pm" should result in "15:22', () => {
-    expect(to24HourClock('03:22pm')).toBe('15:22')
+    expect(dateTimeManager.to24HourClock('03:22pm')).toBe('15:22')
+  })
+
+  test('"11:15am" should result in "11:15', () => {
+    expect(dateTimeManager.to24HourClock('11:15am')).toBe('11:15')
   })
 })
