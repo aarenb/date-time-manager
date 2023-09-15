49fe0a07ebf7e0e48c4ec45c124c938b4c9b23e1
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-15 15:14:21 +0200

    Add start to getFormatedDate

--- src/Date.js
+++ src/Date.js
@@ -3,6 +3,7 @@
  */
 export class Date {
   #yyyy
+  #yy
   #mm
   #dd
   /**
@@ -14,7 +15,33 @@ export class Date {
    */
   constructor (year, month, day) {
     this.#yyyy = year
+    this.#yy = `${year.charAt(2)}${year.charAt(3)}`
     this.#mm = month
     this.#dd = day
+  }
+
+  /**
+   * Returns the date in a certain format.
+   *
+   * @param {string} format - The format to return the date in, (ex. dd/mm/yy).
+   * @returns {string} The formated date.
+   */
+  getFormatedDate (format) {
+    let date = ''
+    switch (format) {
+      case 'dd/mm/yyyy':
+        date = `${this.#dd}/${this.#mm}/${this.#yyyy}`
+        break
+      case 'dd/mm/yy':
+        date = `${this.#dd}/${this.#mm}/${this.#yy}`
+        break
+      case 'yyyy/mm/dd':
+        date = `${this.#yyyy}/${this.#mm}/${this.#dd}`
+        break
+      case 'yy/mm/dd':
+        date = `${this.#yy}/${this.#mm}/${this.#dd}`
+        break
+    }
+    return date
   }
 }
