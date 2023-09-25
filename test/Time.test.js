4be21fdeee039017ba6cc233aabf6ffbe0c3d093
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-25 10:38:21 +0200

    Add more tests for addTime in Time class

--- test/Time.test.js
+++ test/Time.test.js
@@ -17,4 +17,14 @@ describe('addTime', () => {
     testTime.addTime(1, 0)
     expect(testTime.get24HourClock()).toBe('14:45')
   })
+
+  test('0, 60 should result in 15:45', () => {
+    testTime.addTime(0, 60)
+    expect(testTime.get24HourClock()).toBe('15:45')
+  })
+
+  test('10, 3 should result in 01:48', () => {
+    testTime.addTime(10, 3)
+    expect(testTime.get24HourClock()).toBe('01:48')
+  })
 })
