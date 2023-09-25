b746aa31fec8477a0688f82b8cda2e9ea4bfaf42
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-09-25 12:17:52 +0200

    Add another test for Date's removeTime

--- test/Date.test.js
+++ test/Date.test.js
@@ -65,8 +65,13 @@ describe('removeTime', () => {
     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('31/11/2008')
   })
 
-  test('0, 0, 32 should result in 02/11/2008', () => {
+  test('0, 0, 32 should result in 30/10/2008', () => {
     testDate.removeTime(0, 0, 32)
     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('30/10/2008')
+  })
+
+  test('1, 8, 0 should result in 30/02/2007 (although this day technically does not exist)', () => {
+    testDate.removeTime(1, 8, 0)
+    expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('30/02/2007')
   })
 })
