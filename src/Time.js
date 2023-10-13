eecd15a89ba987c7187141ce9203edfc08b67622
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-13 14:19:52 +0200

    Make Time have hour and minute stored as fields

--- src/Time.js
+++ src/Time.js
@@ -4,64 +4,41 @@ import { ExceptionHandler } from './ExceptionHandler'
  * Represents a certain time.
  */
 export class Time {
-  #twelveHourFormat
-  #twentyFourHourFormat
+  #hour
+  #minute
   #exceptionHandler
   /**
    * Initializes a new instance of the Time class.
    *
-   * @param {string} time - The time
-   * @param {number} format - The format of the entered time (12/24)
+   * @param {number} hour - The hour (as the 24 hour clock).
+   * @param {number} minute - The minute.
    */
-  constructor (time, format) {
+  constructor (hour, minute) {
     this.#exceptionHandler = new ExceptionHandler()
 
-    this.#exceptionHandler.guardAgainstNotString(time)
-    this.#exceptionHandler.guardAgainstNotNumber(format)
-    this.#checkIfValidFormat(format)
+    this.#exceptionHandler.guardAgainstNotNumber(hour)
+    this.#exceptionHandler.guardAgainstNotNumber(minute)
 
-    this.#setTimeIn12HourClockFormat(time, format)
-    this.#setTimeIn24HourClockFormat(time, format)
+    this.#setHour(hour)
+    this.#setMinute(minute)
   }
 
   /**
-   * Checks if the passed argument is a valid format (12 or 24).
+   * Sets the hour.
    *
-   * @param {number} format - The argument to check.
+   * @param {number} hour - The hour to set.
    */
-  #checkIfValidFormat (format) {
-    if (format !== 12 & format !== 24) {
-      throw new TypeError('The passed argument is not a valid format (12/24)')
-    }
-    // TODO: Add this in exception handler??
+  #setHour (hour) {
+    this.#hour = hour
   }
 
   /**
-   * Sets the time in the 12 hour clock format.
+   * Sets the minute.
    *
-   * @param {string} time - The time
-   * @param {number} format - The format of the entered time (12/24)
+   * @param {number} minute - The minute to set.
    */
-  #setTimeIn12HourClockFormat (time, format) {
-    if (format === 12) {
-      this.#twelveHourFormat = time
-    } else {
-      this.#twelveHourFormat = this.#to12HourClock(time)
-    }
-  }
-
-  /**
-   * Sets the time in the 24 hour clock format.
-   *
-   * @param {string} time - The time
-   * @param {number} format - The format of the entered time (12/24)
-   */
-  #setTimeIn24HourClockFormat (time, format) {
-    if (format === 24) {
-      this.#twentyFourHourFormat = time
-    } else {
-      this.#twentyFourHourFormat = this.#to24HourClock(time)
-    }
+  #setMinute (minute) {
+    this.#minute = minute
   }
 
   /**
@@ -70,7 +47,7 @@ export class Time {
    * @returns {string} The time in 12h clock format (hh:mmxm).
    */
   getTimeIn12HourClockFormat () {
-    return this.#twelveHourFormat
+    return this.#to12HourClockFormat(this.#hour, this.#minute)
   }
 
   /**
@@ -79,7 +56,7 @@ export class Time {
    * @returns {string} The time in 24h clock format (hh:mm).
    */
   getTimeIn24HourClockFormat () {
-    return this.#twentyFourHourFormat
+    return this.#to24HourClockFormat(this.#hour, this.#minute)
   }
 
   /**
@@ -92,10 +69,9 @@ export class Time {
     this.#exceptionHandler.guardAgainstNotNumber(hours)
     this.#exceptionHandler.guardAgainstNotNumber(minutes)
 
-    let currentHour = this.#getHour()
+    let currentHour = this.#hour
 
-    let currentMinute = this.#getMinute()
-
+    let currentMinute = this.#minute
     for (let i = 0; i < minutes; i++) {
       currentMinute++
       if (currentMinute > 59) {
@@ -110,16 +86,6 @@ export class Time {
         currentHour = currentHour - 24
       }
     }
-
-    if (currentHour < 10) {
-      currentHour = `0${currentHour.toString()}`
-    }
-    if (currentMinute < 10) {
-      currentMinute = `0${currentMinute.toString()}`
-    }
-
-    this.#twentyFourHourFormat = `${currentHour}:${currentMinute}`
-    this.#twelveHourFormat = this.#to12HourClock(this.#twentyFourHourFormat)
   }
 
   /**
@@ -130,7 +96,7 @@ export class Time {
   addHours (hours) {
     this.#exceptionHandler.guardAgainstNotNumber(hours)
 
-    let currentHour = this.#getHour()
+    let currentHour = this.#hour
 
     for (let i = 0; i < hours; i++) {
       currentHour++
@@ -138,24 +104,8 @@ export class Time {
         currentHour = currentHour - 24
       }
     }
-  }
 
-  /**
-   * Gets the time's hour in the 24 hour clock format.
-   *
-   * @returns {number} The hour.
-   */
-  #getHour () {
-    return Number(`${this.#twentyFourHourFormat.charAt(0)}${this.#twentyFourHourFormat.charAt(1)}`)
-  }
-
-  /**
-   * Gets the time's minute.
-   *
-   * @returns {number} The minute.
-   */
-  #getMinute () {
-    return Number(`${this.#twentyFourHourFormat.charAt(3)}${this.#twentyFourHourFormat.charAt(4)}`)
+    this.#setHour(currentHour)
   }
 
   /**
@@ -168,8 +118,8 @@ export class Time {
     this.#exceptionHandler.guardAgainstNotNumber(hours)
     this.#exceptionHandler.guardAgainstNotNumber(minutes)
 
-    let currentHour = this.#getHour()
-    let currentMinute = this.#getMinute()
+    let currentHour = this.#hour
+    let currentMinute = this.#minute
 
     for (let i = 0; i < minutes; i++) {
       currentMinute--
@@ -192,134 +142,92 @@ export class Time {
     if (currentMinute < 10) {
       currentMinute = `0${currentMinute.toString()}`
     }
-
-    this.#twentyFourHourFormat = `${currentHour}:${currentMinute}`
-    this.#twelveHourFormat = this.#to12HourClock(this.#twentyFourHourFormat)
   }
 
   /**
    * Formats time into the 24 hour clock.
    *
-   * @param {string} time - The time to format in 12 hour clock (hh:mmxm).
+   * @param {number} hour - The time's hour.
+   * @param {number} minute - The time's minute.
    * @returns {string} The time formated in the 24 hour clock (hh:mm).
    */
-  #to24HourClock (time) {
-    let newHH = ''
-    const hh = `${time.charAt(0)}${time.charAt(1)}`
-    const mm = `${time.charAt(3)}${time.charAt(4)}`
-    const xm = `${time.charAt(5)}${time.charAt(6)}`
-
-    if (xm === 'am') {
-      if (hh === '12') { // TODO: break this out?
-        newHH = '00'
-      } else {
-        newHH = hh
-      }
-      return `${newHH}:${mm}`
-    } else if (xm === 'pm') {
-      switch (hh) { // TODO: break this out?
-        case '01':
-          newHH = '13'
-          break
-        case '02':
-          newHH = '14'
-          break
-        case '03':
-          newHH = '15'
-          break
-        case '04':
-          newHH = '16'
-          break
-        case '05':
-          newHH = '17'
-          break
-        case '06':
-          newHH = '18'
-          break
-        case '07':
-          newHH = '19'
-          break
-        case '08':
-          newHH = '20'
-          break
-        case '09':
-          newHH = '21'
-          break
-        case '10':
-          newHH = '22'
-          break
-        case '11':
-          newHH = '23'
-          break
-        case '12':
-          newHH = '12'
-          break
-      }
-      return `${newHH}:${mm}`
+  #to24HourClockFormat (hour, minute) {
+    if (hour < 10) {
+      hour = `0${hour.toString()}`
     } else {
-      throw new TypeError('The passed argument is not a valid 12h clock timestamp.')
+      hour = hour.toString()
     }
+
+    if (minute < 10) {
+      minute = `0${minute.toString()}`
+    } else {
+      minute = minute.toString()
+    }
+
+    return `${hour}:${minute}`
   }
 
   /**
    * Formats time into the 12 hour clock.
    *
-   * @param {string} time - The time to format in the 24 hour clock (hh:mm).
+   * @param {number} hour - The time's hour.
+   * @param {number} minute - The time's minute.
    * @returns {string} The time formatted in 12 hour clock (hh:mmxm).
    */
-  #to12HourClock (time) {
-    // TODO: dublicated code, break out???
-    // TODO: Check if hh and mm are correct????
-    let newHH = ''
-    const hh = `${time.charAt(0)}${time.charAt(1)}`
-    const mm = `${time.charAt(3)}${time.charAt(4)}`
+  #to12HourClockFormat (hour, minute) {
+    let newHour = ''
+    minute = minute.toString()
 
-    if (parseInt(hh) < 12 && parseInt(hh) > 0) {
-      newHH = hh
-      return `${newHH}:${mm}am`
-    } else if (hh === '00') {
-      newHH = 12
-      return `${newHH}:${mm}am`
+    if (hour < 12 && hour > 0) {
+      if (hour < 10) {
+        newHour = `0${hour.toString()}`
+      } else {
+        newHour = hour.toString()
+      }
+      return `${newHour}:${minute}am`
+    } else if (hour === 0) {
+      newHour = 12
+      return `${newHour}:${minute}am`
     } else {
-      switch (hh) { // TODO: break this out?
-        case '12':
-          newHH = hh
+      switch (hour) { // TODO: break this out?
+        case 12:
+          newHour = hour.toString()
           break
-        case '13':
-          newHH = '01'
+        case 13:
+          newHour = '01'
           break
-        case '14':
-          newHH = '02'
+        case 14:
+          newHour = '02'
           break
-        case '15':
-          newHH = '03'
+        case 15:
+          newHour = '03'
           break
-        case '16':
-          newHH = '04'
+        case 16:
+          newHour = '04'
           break
-        case '17':
-          newHH = '05'
+        case 17:
+          newHour = '05'
           break
-        case '18':
-          newHH = '06'
+        case 18:
+          newHour = '06'
           break
-        case '19':
-          newHH = '07'
+        case 19:
+          newHour = '07'
           break
-        case '20':
-          newHH = '08'
+        case 20:
+          newHour = '08'
           break
-        case '21':
-          newHH = '09'
+        case 21:
+          newHour = '09'
           break
-        case '22':
-          newHH = '10'
+        case 22:
+          newHour = '10'
           break
-        case '23':
-          newHH = '11'
+        case 23:
+          newHour = '11'
           break
       }
-      return `${newHH}:${mm}pm`
+      return `${newHour}:${minute}pm`
     }
   }
 }
