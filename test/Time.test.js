017d29954278b4e7c47e657e08d45603b754642c
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-17 13:08:19 +0200

    Add addMinutes automatic tests

--- test/Time.test.js
+++ test/Time.test.js
@@ -11,3 +11,23 @@ describe('Get formated time', () => {
     expect(testTime.getTimeIn24HourClockFormat()).toBe('13:45')
   })
 })
+
+describe('addMinutes', () => {
+  test('10 should result in 13:55', () => {
+    const testTime = new Time(13, 45)
+    testTime.addMinutes(10)
+    expect(testTime.getTimeIn24HourClockFormat()).toBe('13:55')
+  })
+
+  test('15 should result in 14:00', () => {
+    const testTime = new Time(13, 45)
+    testTime.addMinutes(15)
+    expect(testTime.getTimeIn24HourClockFormat()).toBe('14:00')
+  })
+
+  test('30 should result in 00:00', () => {
+    const testTime = new Time(23, 30)
+    testTime.addMinutes(30)
+    expect(testTime.getTimeIn24HourClockFormat()).toBe('00:00')
+  })
+})
