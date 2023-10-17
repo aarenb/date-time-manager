464cf2fc473ce7aaa9398916e18a50402dc0e147
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-10-17 09:50:46 +0200

    Add describe for all tests in Date.test

--- test/Date.test.js
+++ test/Date.test.js
@@ -75,41 +75,18 @@ describe('addMonths', () => {
   // TODO: Add tests
 })
 
-// describe('addTime', () => {
-//   test('1, 0, 0 should result in 11/07/2004', () => {
-//     testDate.addTime(1, 0, 0)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('11/07/2004')
-//   })
+describe('addYears', () => {
+  // TODO: Add tests
+})
 
-//   test('0, 7, 0 should result in 11/02/2005', () => {
-//     testDate.addTime(0, 7, 0)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('11/02/2005')
-//   })
+describe('subtractDays', () => {
+  // TODO: Add tests
+})
 
-//   test('0, 0, 22 should result in 05/03/2005', () => {
-//     testDate.addTime(0, 0, 22)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('05/03/2005')
-//   })
+describe('subtractMonths', () => {
+  // TODO: Add tests
+})
 
-//   test('5, 2, 26 should result in 31/05/2010', () => {
-//     testDate.addTime(5, 2, 26)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('31/05/2010')
-//   })
-// })
-
-// describe('removeTime', () => {
-//   test('1, 0, 0 should result in 31/05/2009', () => {
-//     testDate.removeTime(1, 0, 0)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('31/05/2009')
-//   })
-
-//   test('0, 6, 0 should result in 31/12/2008', () => {
-//     testDate.removeTime(0, 6, 0)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('31/11/2008')
-//   })
-
-//   test('0, 0, 32 should result in 30/10/2008', () => {
-//     testDate.removeTime(0, 0, 32)
-//     expect(testDate.getFormatedDate('dd/mm/yyyy')).toBe('30/10/2008')
-//   })
-// })
+describe('subtractYears', () => {
+  // TODO: Add tests
+})
